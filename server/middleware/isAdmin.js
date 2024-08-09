86d197c89c6db7a87657982bd11e89b8a40e3ca9
internArshea <arshea.intern@devsinc.com>
2024-08-09 19:14:27 +0500

    Checking Admin

---
+++ server/middleware/isAdmin.js
@@ -0,0 +1,19 @@
+import User from '../models/UserModel.js'
+
+const isAdmin = async (req, res, next)=>{
+    try{
+        const adminUsers = await User.find({ isAdmin: true });
+        const isUserAdmin =  adminUsers.some((user)=> user.id === req.user);
+        if(isUserAdmin){
+            req.id = req.user;
+            next();
+        }
+        else{
+            res.status(403).json({message: 'Not Authorized'});
+        }
+    }catch(err){
+        res.status(422).json('No Admin');
+    }
+}
+
+export default isAdmin
