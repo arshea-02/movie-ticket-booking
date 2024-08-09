526114d5233f307bb5cd9d70e907b9de7b179a1d
internArshea <arshea.intern@devsinc.com>
2024-08-09 19:10:33 +0500

    Updating creatID

---
+++ server/utils/createID.js
@@ -0,0 +1,11 @@
+
+const createID = async (ojb_name)=>{
+    const date = new Date(Date.now()).getFullYear();
+    const extractCapital = await ojb_name.match(/[A-Z]/g);
+    const joinedCapital = extractCapital ? extractCapital.join(" ") : ""; 
+
+    const obj_id = (date.toString(10) + joinedCapital.replace(" ", "") +  Math.floor(Math.random()*900).toString(10));
+    return obj_id;
+}
+
+export default createID;
